42562bb7734dccb30156b45e6d3cb6685f8ef9c9
NonPlayer95 <130722174+Nonplayer95@users.noreply.github.com>
2025-11-03 15:03:09 +0900

    Initialize dev branch

---
+++ Source/ProjecD/ProjecD.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ProjecD.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ProjecD, "ProjecD" );

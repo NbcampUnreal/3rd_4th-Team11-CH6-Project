b0e1d092718b8da33dba523689b8ddd4d4cdbf58
NonPlayer95 <130722174+Nonplayer95@users.noreply.github.com>
2025-11-04 02:42:15 +0900

    Add RBaseOutGameWidget class and header files

---
+++ Source/ProjecD/UI/Base/RBaseOutGameWidget.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "RBaseOutGameWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECD_API URBaseOutGameWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};

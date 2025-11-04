e620dfe15f1290e167b000b31bfb1f02c216a953
NonPlayer95 <130722174+Nonplayer95@users.noreply.github.com>
2025-11-05 00:48:49 +0900

    UROutGameUIManager 및 RBaseOutGameWidget 구현을 통한 UI 관리 시스템 추가

--- Source/ProjecD/UI/Base/RBaseOutGameWidget.cpp
+++ Source/ProjecD/UI/Base/RBaseOutGameWidget.cpp
@@ -1,2 +1,35 @@
 #include "UI/Base/RBaseOutGameWidget.h"
 
+URBaseOutGameWidget::URBaseOutGameWidget(const FObjectInitializer& ObjectInitializer)
+	:Super(ObjectInitializer)
+{
+}
+
+void URBaseOutGameWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+	UE_LOG(LogTemp,Log,TEXT("BaseOutGameWidget is created"));
+}
+
+void URBaseOutGameWidget::ShowUI()
+{
+	//화면에 추가
+	AddToViewport();
+
+	//입력 모드를 UI전용으로 변경
+	if (APlayerController* PC=GetOwningPlayer())
+	{
+		FInputModeUIOnly InputMode;
+		InputMode.SetWidgetToFocus(TakeWidget());
+		PC->SetInputMode(InputMode);
+		PC->bShowMouseCursor=true;
+	}
+	UE_LOG(LogTemp,Log,TEXT("%s 표시됨"),*GetClass()->GetName());
+}
+
+void URBaseOutGameWidget::HideUI()
+{
+	//화면에서 제거
+	RemoveFromParent();
+	UE_LOG(LogTemp, Log, TEXT("%s 숨김"), *GetClass()->GetName());
+}
--- Source/ProjecD/UI/Manager/ROutGameUIManager.h
+++ Source/ProjecD/UI/Manager/ROutGameUIManager.h
@@ -5,11 +5,65 @@
 #include "ROutGameUIManager.generated.h"
 
 /**
- * 
+ * Outgame UI 생성, 전환, 스택 관리 클래스
  */
 UCLASS()
 class PROJECD_API UROutGameUIManager : public UObject
 {
 	GENERATED_BODY()
+
+public:
+	UROutGameUIManager(); //초기화
+
+	// PlayerController 설정 및 GameInstance가 Init()에서 호출
+	void SetPlayerController(APlayerController* PC);
+
+	// UI 관리 함수 //
+	
+	// UI위젯 생성 및 화면 표시
+	UFUNCTION(BlueprintCallable,Category="UI")
+	void ShowUI(TSubclassOf<URBaseOutGameWidget> WidgetClass);
+
+	//현재 활성화된 UI 숨기기
+	UFUNCTION(BlueprintCallable,Category="UI")
+	void HideCurrentUI();
+
+	//이전 UI로 돌아가기(스택에서 pop)
+	UFUNCTION(BlueprintCallable,Category="UI")
+	void GoBackToPreviousUI();
+
+	//모든 UI 제거 & 스택 초기화
+	UFUNCTION(BlueprintCallable,Category="UI")
+	void ClearAllUI();
+
+	//Getter함수
+
+	// 현재 활성화된 UI반환
+	UFUNCTION(BlueprintCallable,Category="UI")
+	URBaseOutGameWidget* GetCurrentWidget() const {return CurrentWidget;}
+	
+private:
+	//내부 헬퍼함수
+	// 위젯을 생성하거나 캐시에서 가져옴
+	URBaseOutGameWidget* GetOrCreateWidget(TSubclassOf<URBaseOutGameWidget> WidgetClass);
+
+	//맴버 변수
+
+	// 현재 활성화된 UI위젯
+	UPROPERTY()
+	URBaseOutGameWidget* CurrentWidget;
+
+	//UI 스택 (뒤로가기 기능관련)
+	UPROPERTY()
+	TArray<URBaseOutGameWidget*> WidgetStack;
+
+	//생성된 위젯 캐시 (재사용) - Key: 위젯 클래스, Value: 위젯 인스턴스/
+	UPROPERTY()
+	TMap<TSubclassOf<URBaseOutGameWidget>,URBaseOutGameWidget*> CachedWidgets;
+
+	//PlayerController참조(입력 모드 변경용)
+	UPROPERTY()
+	APlayerController* PlayerController;
+
 	
 };
